e5525d09b61063306fc7e2680bcf1903e70a187d
sgaron-cse <sgaron.cse@gmail.com>
2017-03-07 10:19:51 -0500

    Make Unpacker an Extraction service.

--- unpacker.py
+++ unpacker.py
@@ -1,4 +1,5 @@
 import os
+import posixpath
 import subprocess
 
 from collections import namedtuple
@@ -10,16 +11,16 @@ from assemblyline.al.service.base import ServiceBase
 PACKER_UNKNOWN = 'unknown'
 PACKER_UPX = 'upx'
 
-UnpackResult = namedtuple('UnpackResult', ['ok', 'filepath', 'meta'])
+UnpackResult = namedtuple('UnpackResult', ['ok', 'localpath', 'displayname', 'meta'])
 
 
 class Unpacker(ServiceBase):
     SERVICE_ACCEPTS = 'executable/*'
-    SERVICE_CATEGORY = "Static Analysis"
+    SERVICE_CATEGORY = "Extraction"
     SERVICE_DESCRIPTION = "This service unpacks UPX packed executables for further analysis."
     SERVICE_ENABLED = True
-    SERVICE_REVISION = ServiceBase.parse_revision('$Id: 3da3b153a64ab046981d918f57d081d6c6eb3d04 $')
-    SERVICE_STAGE = 'SECONDARY'
+    SERVICE_REVISION = ServiceBase.parse_revision('$Id$')
+    SERVICE_STAGE = 'EXTRACT'
     SERVICE_VERSION = '1'
     SERVICE_CPU_CORES = 0.5
     SERVICE_RAM_MB = 256
@@ -37,13 +38,14 @@ class Unpacker(ServiceBase):
     def execute(self, request):
         request.result = Result()
         uresult = self._unpack(request, ['upx'])
-        if uresult.ok and uresult.filepath:
-            request.add_extracted(uresult.filepath, 'Unpacked from %s' % request.srl)
-            request.result.add_section(ResultSection(SCORE.INFO,
-                                                     "%s successfully unpacked!" % (os.path.basename(uresult.filepath)),
+        if uresult.ok and uresult.localpath:
+            request.add_extracted(uresult.localpath, 'Unpacked from %s' % request.srl, display_name=uresult.displayname)
+            request.result.add_section(ResultSection(SCORE.NULL, "%s successfully unpacked!" %
+                                                     (os.path.basename(uresult.displayname)),
                                        self.SERVICE_CLASSIFICATION))
+            request.drop()
 
-    def _unpack_upx(self, packedfile, outputpath):
+    def _unpack_upx(self, packedfile, outputpath, displayname):
         # Test the file to see if UPX agrees with our identification.
         p = subprocess.Popen(
             (self.upx_exe, '-t', packedfile),
@@ -60,25 +62,21 @@ class Unpacker(ServiceBase):
 
             if 'Unpacked 1 file' in stdout:
                 # successfully unpacked.
-                return UnpackResult(True, outputpath, {'stdout': stdout[:1024]})
+                return UnpackResult(True, outputpath, displayname, {'stdout': stdout[:1024]})
         else:
             self.log.info('UPX extractor said this file was not UPX packed:\n%s\n%s',
                           stdout[:1024], stderr[:1024])
         # UPX unpacking is failure prone due to the number of samples that are identified as UPX
         # but are really some minor variant. For that reason we can't really fail the result
         # every time upx has problems with a file.
-        return UnpackResult(True, None, None)
+        return UnpackResult(True, None, None, None)
 
     def _unpack(self, request, packer_names):
-        packer = PACKER_UNKNOWN
         for name in packer_names:
             if 'upx' in name.lower():
-                packer = PACKER_UPX
-                break
-        # for right now we only support upx.
-        if packer != PACKER_UPX:
-            return UnpackResult(True, None, None)
+                packedfile = request.download()
+                unpackedfile = packedfile + '.unUPX'
+                displayname = posixpath.basename(request.path) + '.unUPX'
+                return self._unpack_upx(packedfile, unpackedfile, displayname)
 
-        packedfile = request.download()
-        unpackedfile = packedfile + '.unpacked'
-        return self._unpack_upx(packedfile, unpackedfile)
+        return UnpackResult(True, None, None, None)
