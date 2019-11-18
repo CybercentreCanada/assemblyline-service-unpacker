d570c7232db4e08e7c2b68d3dcde1bed6fd3f0e7
Angela Deaguiar <angela.deaguiar@165gc.onmicrosoft.com>
2019-11-18 13:10:36 -0400

    V4 Conversion

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM cccs/assemblyline-v4-service-base:latest
+
+ENV SERVICE_PATH unpacker.Unpacker
+
+#RUN apt-get update && apt-get install -y \
+    #upx-ucl
+
+# Switch to assemblyline user
+USER assemblyline
+
+# Copy Unpacker service code
+WORKDIR /opt/al_service
+COPY . .
--- unpacker.py
+++ unpacker.py
@@ -1,11 +1,9 @@
 import os
-import subprocess
-
+from subprocess import PIPE, Popen
 from collections import namedtuple
 
-from assemblyline.al.common.result import Result, ResultSection
-from assemblyline.al.common.result import SCORE
-from assemblyline.al.service.base import ServiceBase
+from assemblyline_v4_service.common.base import ServiceBase
+from assemblyline_v4_service.common.result import Result, ResultSection
 
 PACKER_UNKNOWN = 'unknown'
 PACKER_UPX = 'upx'
@@ -14,56 +12,34 @@ UnpackResult = namedtuple('UnpackResult', ['ok', 'localpath', 'displayname', 'me
 
 
 class Unpacker(ServiceBase):
-    SERVICE_ACCEPTS = 'executable/*'
-    SERVICE_CATEGORY = "Static Analysis"
-    SERVICE_DESCRIPTION = "This service unpacks UPX packed executables for further analysis."
-    SERVICE_ENABLED = True
-    SERVICE_REVISION = ServiceBase.parse_revision('$Id$')
-    SERVICE_STAGE = 'SECONDARY'
-    SERVICE_VERSION = '1'
-    SERVICE_CPU_CORES = 0.5
-    SERVICE_RAM_MB = 256
 
-    SERVICE_DEFAULT_CONFIG = {
-        'UPX_EXE': r'/usr/bin/upx',
-    }
-
-    def __init__(self, cfg=None):
-        super(Unpacker, self).__init__(cfg)
-        self.upx_exe = self.cfg.get('UPX_EXE')
+    def __init__(self, config=None):
+        super(Unpacker, self).__init__(config)
+        self.upx_exe = self.config.get('upx_exe')
         if not os.path.exists(self.upx_exe):
-            raise Exception('UPX executable not found on system: %s', self.upx_exe)
+            raise Exception(f'UPX executable not found on system: {self.upx_exe}')
 
     def execute(self, request):
         request.result = Result()
         uresult = self._unpack(request, ['upx'])
         if uresult.ok and uresult.localpath:
-            request.add_extracted(uresult.localpath, 'Unpacked from %s' % request.srl, display_name=uresult.displayname)
-            request.result.add_section(ResultSection(SCORE.NULL, "%s successfully unpacked!" %
-                                                     (os.path.basename(uresult.displayname)),
-                                       self.SERVICE_CLASSIFICATION))
+            request.add_extracted(uresult.localpath, f'Unpacked from {request.sha256}', uresult.displayname)
+            request.result.add_section(ResultSection(f"{os.path.basename(uresult.displayname)} successfully unpacked!"))
 
     def _unpack_upx(self, packedfile, outputpath, displayname):
         # Test the file to see if UPX agrees with our identification.
-        p = subprocess.Popen(
-            (self.upx_exe, '-t', packedfile),
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE)
-        (stdout, stderr) = p.communicate()
+        stdout, stderr = Popen((self.upx_exe, '-t', packedfile),
+            stdout=PIPE, stderr=PIPE).communicate()
 
-        if '[OK]' in stdout and 'Tested 1 file' in stdout:
-            p = subprocess.Popen(
-                (self.upx_exe, '-d', '-o', outputpath, packedfile),
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE)
-            (stdout, stderr) = p.communicate()
+        if b'[OK]' in stdout and b'Tested 1 file' in stdout:
+             stdout, stderr = Popen((self.upx_exe, '-d', '-o', outputpath, packedfile),
+                stdout=PIPE, stderr=PIPE).communicate()
 
-            if 'Unpacked 1 file' in stdout:
+             if b'Unpacked 1 file' in stdout:
                 # successfully unpacked.
                 return UnpackResult(True, outputpath, displayname, {'stdout': stdout[:1024]})
         else:
-            self.log.info('UPX extractor said this file was not UPX packed:\n%s\n%s',
-                          stdout[:1024], stderr[:1024])
+            self.log.info(f'UPX extractor said this file was not UPX packed:\n{stderr[:1024]}\n{stderr[:1024]}')
         # UPX unpacking is failure prone due to the number of samples that are identified as UPX
         # but are really some minor variant. For that reason we can't really fail the result
         # every time upx has problems with a file.
@@ -72,9 +48,9 @@ class Unpacker(ServiceBase):
     def _unpack(self, request, packer_names):
         for name in packer_names:
             if 'upx' in name.lower():
-                packedfile = request.download()
+                packedfile = request.file_path
                 unpackedfile = packedfile + '.unUPX'
-                displayname = os.path.basename(request.path) + '.unUPX'
+                displayname = os.path.basename(request.file_path) + '.unUPX'
                 return self._unpack_upx(packedfile, unpackedfile, displayname)
 
         return UnpackResult(True, None, None, None)
